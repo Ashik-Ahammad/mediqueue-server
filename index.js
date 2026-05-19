6d3ea1043c40becdea9e2daaa4cce3ceb38b6770
Ashik-Ahammad <ashik19ahammad@gmail.com>
2026-05-19 19:44:08 +0600

    Add Booking api

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     await client.connect();
     const db = client.db("mediqueue");
     const tutorCollection = db.collection("tutors");
+    const bookingCollection = db.collection("bookings");
 
     // api's
 
@@ -45,28 +46,98 @@ async function run() {
     });
 
     // Get This Tutor Data
-    app.get("/tutors", async(req, res) => {
+    app.get("/tutors", async (req, res) => {
       try {
         const result = await tutorCollection.find().toArray();
         res.json(result);
       } catch (err) {
         res.status(500).json({ message: "Server error", error: err.message });
       }
-    })
+    });
+
+    // Get most updated featured Tutor data using limit and sort
+    app.get("/featured-tutors", async (req, res) => {
+      try {
+        const result = await tutorCollection
+          .aggregate([{ $sort: { _id: -1 } }, { $limit: 6 }])
+          .toArray();
+        res.json(result);
+      } catch (err) {
+        res.status(500).json({ message: "Server error", error: err.message });
+      }
+    });
 
     //Get Single tutor details data
-    app.get("/tutors/:id", async(req, res) => {
+    app.get("/tutors/:id", async (req, res) => {
       try {
-        const {id} = req.params;
+        const { id } = req.params;
         const result = await tutorCollection.findOne({
-          _id: new ObjectId(id)
+          _id: new ObjectId(id),
         });
         res.json(result);
       } catch (err) {
         res.status(500).json({ message: "Server error", error: err.message });
       }
-    })
+    });
 
+    // Post Booking data
+    app.post("/bookings", async (req, res) => {
+      try {
+        const bookingsData = req.body;
+        const tutorId = bookingsData.tutorId;
+
+        // find tutor details from db
+        const tutor = await tutorCollection.findOne({
+          _id: new ObjectId(tutorId),
+        });
+        if (!tutor) {
+          return res.status(404).json({ message: "Tutor not found" });
+        }
+
+        // session date restrictions
+        const currentDate = new Date();
+        const sessionDate = new Date(tutor.startDate);
+
+        // accurate day comparison
+        currentDate.setHours(0, 0, 0, 0);
+        sessionDate.setHours(0, 0, 0, 0);
+
+        if (currentDate < sessionDate) {
+          return res
+            .status(400)
+            .json({ message: "Booking is not available yet for this tutor" });
+        }
+
+        // validate available slots
+        const currentSlotNum = parseInt(tutor.totalSlot);
+        if (isNaN(currentSlotNum) || currentSlotNum <= 0) {
+          return res
+            .status(400)
+            .json({
+              message:
+                "This session is fully booked. You can’t join at the moment.",
+            });
+        }
+
+        // save booking record to the db
+        const result = await bookingCollection.insertOne(bookingsData);
+
+        // automatically decrease available slot count
+        if (result.insertedId) {
+
+          const newSlotValue = (currentSlotNum - 1).toString();
+
+          await tutorCollection.updateOne(
+            { _id: new ObjectId(tutorId) },
+            { $set: { totalSlot: newSlotValue } },
+          );
+        }
+
+        res.json({ message: "Booking successful", result });
+      } catch (err) {
+        res.status(500).json({ message: "Server error", error: err.message });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Ping pong. Server successfully connected to MongoDB!");
